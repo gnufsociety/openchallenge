b3cfed0d68786792538f7301bd270d59da2c848e
Luigi Pan <luigi.pan95@gmail.com>
2017-01-10 22:45:16 +0100

    boh

--- android-app/OpenChallenge/app/src/main/java/com/gnufsociety/openchallenge/MainActivity.java
+++ android-app/OpenChallenge/app/src/main/java/com/gnufsociety/openchallenge/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("lol");
-        System.out.println("Tiemmodif");
+        System.out.println("Tiemmodify");
     }
 }
