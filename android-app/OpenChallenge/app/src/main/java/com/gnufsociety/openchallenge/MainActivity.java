6fa48f1993ad7820283ccd2424bd9aaeccea6a9c
scemodicecosa <leonardosagratella@gmail.com>
2017-01-14 15:27:48 +0100

    the bottom bar doesn't move up with the keyboard

--- android-app/OpenChallenge/app/src/main/java/com/gnufsociety/openchallenge/MainActivity.java
+++ android-app/OpenChallenge/app/src/main/java/com/gnufsociety/openchallenge/MainActivity.java
@@ -1,27 +1,19 @@
 package com.gnufsociety.openchallenge;
 
-import android.graphics.Color;
-import android.graphics.PorterDuff;
-import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
-import android.support.v4.view.MenuItemCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
