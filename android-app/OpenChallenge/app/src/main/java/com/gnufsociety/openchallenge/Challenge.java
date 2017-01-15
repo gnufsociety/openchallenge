892ac2fb4e26c577d30fe1e6fc09adf1dbfaebad
scemodicecosa <leonardosagratella@gmail.com>
2017-01-15 23:04:02 +0100

    added more challenges

--- android-app/OpenChallenge/app/src/main/java/com/gnufsociety/openchallenge/Challenge.java
+++ android-app/OpenChallenge/app/src/main/java/com/gnufsociety/openchallenge/Challenge.java
@@ -33,30 +33,28 @@ public class Challenge {
         User u2 = new User("spallas",3.9);
         User u3 = new User("tiem",5);
         User u4 = new User("k",4.3);
-        User u5 = new User("bar tony",2);
+        User u5 = new User("Bastianich",2);
 
         Challenge c1 = new Challenge("Smash the stack", R.drawable.hacking,u1,"Coding", "15 jan 2017", "under the bridge");
-        Challenge c2 = new Challenge("Snowball fight", R.drawable.coke,u2,"Blowing", "23 jan 2017", "sul piatto caldo");
+        Challenge c2 = new Challenge("Snowball fight", R.drawable.snow,u2,"Blowing", "23 jan 2017", "Via bufus 117");
         Challenge c3 = new Challenge("Pull ups", R.drawable.pullups,u3,"Physical", "17 feb 2017", "Via le mani dal naso 12");
-        Challenge c4 = new Challenge("Drinking contest", R.drawable.pint,u4,"Eat&Drink", "1 mar 2017", "piazza la bomba 8");
-        Challenge c5 = new Challenge("Smash the stack", R.drawable.hacking,u1,"Coding", "15 jan 2017", "under the bridge");
-        Challenge c6 = new Challenge("Smash the stack", R.drawable.hacking,u5,"Coding", "15 jan 2017", "under the bridge");
-        Challenge c7 = new Challenge("Smash the stack", R.drawable.hacking,u3,"Coding", "15 jan 2017", "under the bridge");
-        Challenge c8 = new Challenge("Smash the stack", R.drawable.hacking,u4,"Coding", "15 jan 2017", "under the bridge");
-        Challenge c9 = new Challenge("Smash the stack", R.drawable.hacking,u3,"Coding", "15 jan 2017", "under the bridge");
-        Challenge c10 = new Challenge("Smash the stack", R.drawable.hacking,u2,"Coding", "15 jan 2017", "under the bridge");
+        Challenge c4 = new Challenge("All you can drink", R.drawable.pint,u4,"Eat&Drink", "1 mar 2017", "Piazza la bomba 8");
+        Challenge c5 = new Challenge("Fast typing", R.drawable.typing,u1,"Coding", "25 dec 2017", "Via dotto 23 ");
+        Challenge c6 = new Challenge("MasterChef", R.drawable.cooking,u5,"Cooking", "20 may 2017", "Cracco ce fa na p..");
+        Challenge c7 = new Challenge("Chess", R.drawable.chess,u3,"Game", "7 feb 2017", "Via pyrex 777");
+        Challenge c8 = new Challenge("Briscola", R.drawable.briscola,u4,"Card", "12 may 2017", "Bar del paese");
+        Challenge c9 = new Challenge("Panizzo 2KG", R.drawable.panizzeri,u4,"Eat&Food", "31 jan 2017", "Via L. Bragaglia 71");
 
 
         arr.add(c1);
         arr.add(c2);
         arr.add(c3);
+        arr.add(c9);
         arr.add(c4);
         arr.add(c5);
         arr.add(c6);
         arr.add(c7);
         arr.add(c8);
-        arr.add(c9);
-        arr.add(c10);
 
         return arr;
     }
