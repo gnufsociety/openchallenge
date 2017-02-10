498b49645255fa235b4efb7588020b8cb5a12fc5
spallas <davide.spallaccini@gmail.com>
2017-02-10 15:10:36 +0100

    New routes and bug fixes

--- server/db.js
+++ server/db.js
@@ -3,6 +3,7 @@
  */
 
 var MongoClient = require("mongodb").MongoClient;
+var assert = require('assert');
 
 var state = {
     db: null
@@ -22,12 +23,12 @@ exports.get = function() {
     return state.db;
 };
 
-exports.close = function(done) {
+exports.close = function() {
     if (state.db) {
         state.db.close(function(err, result) {
+            assert.equal(err, null);
             state.db = null;
             state.mode = null;
-            done(err);
         })
     }
 };
--- server/routes/api.js
+++ server/routes/api.js
@@ -13,9 +13,6 @@ db.connect(db_url, function (err, done) {
         process.exit(1);
     } else {
 
-    }
-});
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.send("Hello World!");
@@ -31,23 +28,47 @@ router.post('/newChallenge', function (req, res) {
             assert.equal(null, err);
             res.send(docs[0]);
 
-            db.close();
+
         });
-        
+
     });
 });
 
 router.get('/allChallenges', function (req, res) {
     db.get().collection('challenges').find().toArray(function (err, docs) {
-       assert.equal(null, err);
-       res.json(docs);
+       assert.equal(err, null);
+       // res.json(docs);
+       res.send('[{"_id":"589da3f3e5ac0950b0405b0a","organizer":"3niTtjEL09W4ACIpv9i30Ai4d9A2","name":"lancio di caccole","description":"chi le tira piu lontano","rules":"senza dita dei piedi","image":"lancio_di_caccole208","date":"10/1/2017","location":{"address":"Via Ariosto, 00185 Roma, Italy","lat":41.89054219999999,"long":12.504060699999998}}]');
+
+
     });
 
 });
 
 // USERS
 
+router.post('/newUser', function (req, res) {
+    var body_obj = req.body;
+    db.get().collection('users').insertOne(body_obj, function (err, result) {
+        assert.equal(err, null);
 
+        res.send("Created user:" + body_obj.nickname);
+
+
+    })
+
+});
+
+router.get('allUsers', function (req, res) {
+    db.get().collection('users').find().limit(1).toArray(function (err, docs) {
+        assert.equal(err, null);
+        res.json(docs);
+
+
+    });
+});
+    }
+});
 
 
 module.exports = router;
