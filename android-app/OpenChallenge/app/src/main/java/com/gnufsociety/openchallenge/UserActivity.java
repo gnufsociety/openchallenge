45399e33706a70d078aa9a11c95724872a820d27
Tiem51 <luigi.pan95@gmail.com>
2017-03-04 18:01:16 +0100

    bug per spallas

--- android-app/OpenChallenge/app/src/main/java/com/gnufsociety/openchallenge/UserActivity.java
+++ android-app/OpenChallenge/app/src/main/java/com/gnufsociety/openchallenge/UserActivity.java
@@ -70,10 +70,15 @@ public class UserActivity extends AppCompatActivity {
 
         auth = FirebaseAuth.getInstance();
 
+
         ButterKnife.bind(this);
         Bundle extra = getIntent().getExtras();
         currentUser = (User) extra.getSerializable("currentUser");
         userPic.setImageResource(currentUser.resPic);
+
+        isFollowed = checkFollow();
+        if(isFollowed) fButton.setText("Followed");
+        else fButton.setText("Follow");
 
         FirebaseStorage storage = FirebaseStorage.getInstance();
         StorageReference sref = storage.getReferenceFromUrl("gs://openchallenge-81990.appspot.com");
@@ -104,6 +109,7 @@ public class UserActivity extends AppCompatActivity {
 
         joinedRecycler.setNestedScrollingEnabled(false);
         joinedRecycler.addItemDecoration(dividerItemDecoration);
+
 
         populateChallenges(false);
 
@@ -211,5 +217,15 @@ public class UserActivity extends AppCompatActivity {
             }
         };
         task.execute();
+    }
+
+    public boolean checkFollow(){
+        ApiHelper api = new ApiHelper();
+        ArrayList<User> followedList = api.getFollowed(auth.getCurrentUser().getUid());
+        int length = followedList.size();
+        for(int i=0; i<length; i++){
+            if(followedList.get(i).equals(currentUser)) return true;
+        }
+        return false;
     }
 }
